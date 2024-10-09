34e838291233256358b79105f3da79256ff12ba8
Hacha63 <capitan.hacha@gmail.com>
2024-10-09 17:08:45 +0200

    a

---
+++ src/main/java/com/mycompany/java1/JocCartes.java
@@ -0,0 +1,5 @@
+package com.mycompany.java1;
+
+public class JocCartes {
+
+}
--- src/main/java/com/mycompany/java1/PreguntasMat.java
+++ src/main/java/com/mycompany/java1/PreguntasMat.java
@@ -9,23 +9,29 @@ public class PreguntasMat {
         int pregunta = 0;
 
         System.out.println("Cauntas preguntas quieres? ");
+        int preguntas = scanner.nextInt();
+        double porcentajetotal = preguntas;
+        double porcentaje = 0;
 
-
-        for (int preguntas = scanner.nextInt(); preguntas != 0;preguntas--){
+        for (; preguntas != 0;preguntas--){
             int sim = random.nextInt(4);
             int num1 = random.nextInt(0,10);
             int num2 = random.nextInt(0,10);
+
             double respuesta;
             double correcto;
+
             pregunta ++;
 
             switch (sim){
                 case 0:
                     System.out.print("Pregunta "+pregunta+ ". "+num1+" + "+num2+ " = ");
-                     respuesta = scanner.nextInt();
+                     respuesta = scanner.nextDouble();
                      correcto = num1 + num2;
                     if (respuesta == correcto){
                         System.out.println("CORRECTO");
+                        porcentaje = porcentaje+1;
+
                     }
                     else{
                         System.out.println("INCORRECTO, era "+correcto);
@@ -33,10 +39,11 @@ public class PreguntasMat {
                     break;
                 case 1:
                     System.out.print("Pregunta "+pregunta+ ". "+num1+" - "+num2+ " = ");
-                     respuesta = scanner.nextInt();
+                     respuesta = scanner.nextDouble();
                      correcto = num1 - num2;
                     if (respuesta == correcto){
                         System.out.println("CORRECTO");
+                        porcentaje = porcentaje+1;
                     }
                     else{
                         System.out.println("INCORRECTO, era "+correcto);
@@ -44,10 +51,11 @@ public class PreguntasMat {
                     break;
                 case 2:
                     System.out.print("Pregunta "+pregunta+ ". "+num1+" * "+num2+ " = ");
-                     respuesta = scanner.nextInt();
+                     respuesta = scanner.nextDouble();
                      correcto = num1 * num2;
                     if (respuesta == correcto){
                         System.out.println("CORRECTO");
+                        porcentaje = porcentaje+1;
                     }
                     else{
                         System.out.println("INCORRECTO, era "+correcto);
@@ -56,16 +64,17 @@ public class PreguntasMat {
                 case 3:
                     if (num1 > num2 && num2 != 0) {
                         System.out.print("Pregunta " + pregunta + ". " + num1 + " / " + num2 + " = ");
-                        respuesta = scanner.nextInt();
+                        respuesta = scanner.nextDouble();
                         correcto = num1 / num2;
                     }
                     else{
                         System.out.print("Pregunta "+pregunta+ ". "+num2+" / "+num1+ " = ");
-                        respuesta = scanner.nextInt();
+                        respuesta = scanner.nextDouble();
                         correcto = num2 / num1;
                         }
                     if (respuesta == correcto){
                         System.out.println("CORRECTO");
+                        porcentaje = porcentaje + 1;
                     }
                     else{
                         System.out.println("INCORRECTO, era "+correcto);
@@ -75,5 +84,7 @@ public class PreguntasMat {
             }
 
         }
+
+        System.out.println("El porcentaje és de: "+porcentaje/porcentajetotal*100+"%");
     }
 }
