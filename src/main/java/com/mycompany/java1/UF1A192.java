de436353b7d41332880e44349e932ada9285fda5
Hacha63 <capitan.hacha@gmail.com>
2024-11-07 21:21:29 +0100

    a

--- src/main/java/com/mycompany/java1/UF1A192.java
+++ src/main/java/com/mycompany/java1/UF1A192.java
@@ -3,24 +3,77 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class UF1A192 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int [] num = new int[6];
-        int [] num1 = new int[6];
-        boolean end = true;
+        int[] numJugador = new int[6];
+        int[] numLoteria = new int[6];
+        boolean ganador = false;
 
-        for (int i = 0; i<6; i++){
-            System.out.println("Num" +i+"\nIntroduce el numero deseado:");
-            num[i] = scanner.nextInt();
-            num1[i] = random.nextInt(1,49);
-        }
-        for (int i = 0; i<6;i++){
-            while (num1[i] == num1[i+1]){
-                num1[i] = random.nextInt(1,49);
+        System.out.println("Introduce 6 números únicos entre 1 y 49 para tu apuesta:");
+        for (int i = 0; i < 6; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            int numero = scanner.nextInt();
+
+            boolean repetit = false;
+            if (numero < 1 || numero > 49) {
+                System.out.println("Número fuera de rango. Introduce un número entre 1 y 49.");
+                i--;
+                continue;
             }
 
+            for (int j = 0; j < i; j++) {
+                if (numJugador[j] == numero) {
+                    System.out.println("Número repetido. Introduce un número diferente.");
+                    repetit = true;
+                    break;
+                }
+            }
+            if (repetit) {
+                i--;
+            } else {
+                numJugador[i] = numero;
+            }
+        }
+
+        for (int i = 0; i < 6; i++) {
+            int numero = random.nextInt(49) + 1;
+
+            boolean repetit = false;
+            for (int j = 0; j < i; j++) {
+                if (numLoteria[j] == numero) {
+                    repetit = true;
+                    break;
+                }
+            }
+            if (repetit) {
+                i--;
+            } else {
+                numLoteria[i] = numero;
+            }
+        }
+
+        System.out.println("\nCombinación ganadora de la lotería:");
+        for (int num : numLoteria) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        int encerts = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (numJugador[i] == numLoteria[j]) {
+                    System.out.println("Número acertado: " + numJugador[i]);
+                    encerts++;
+                    break;
+                }
+            }
+        }
+        if (encerts > 0) {
+            System.out.println("Has acertado " + encerts + " número(s).");
+        } else {
+            System.out.println("No has ganado nada :(");
         }
     }
 }
