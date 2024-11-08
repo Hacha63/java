3e3ba35052f047195a4f53b6d6d10d0f11e52ece
Hacha63 <capitan.hacha@gmail.com>
2024-11-08 20:19:08 +0100

    a

---
+++ src/main/java/com/mycompany/java1/UF1A20.java
@@ -0,0 +1,12 @@
+package com.mycompany.java1;
+import java.util.Random;
+import java.util.Scanner;
+
+public class UF1A20 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+
+    }
+}
