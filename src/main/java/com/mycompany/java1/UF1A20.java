b81d00b833a27cdb022b753ccc28aa61280b8674
Hacha63 <capitan.hacha@gmail.com>
2024-11-14 20:04:53 +0100

    a

--- src/main/java/com/mycompany/java1/UF1A20.java
+++ src/main/java/com/mycompany/java1/UF1A20.java
@@ -7,20 +7,19 @@ public class UF1A20 {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        double [] tortugas = new double[3];
+        double [] tortugas = new double[2];
         boolean victoria = false;
 
         System.out.println("Presiona ENTER para continuar jugando");
         while (!victoria){
-            for (int i = 0; i<3;i++){
-                for (int j = 0; j<3; j++){
+            for (int i = 0; i<2;i++){
+                for (int j = 0; j<2; j++){
                     System.out.println("Tortuga "+(j+1)+" posición: "+tortugas[j]);
                 }
                 System.out.println("");
                 double num = random.nextInt(1,3);
                 int obstaculo = random.nextInt(6);
                 String continuar = scanner.nextLine();
-
                 System.out.println("Turno tortuga: "+(i+1)+"\n");
                 switch (obstaculo) {
                     case 0:
@@ -45,7 +44,6 @@ public class UF1A20 {
                         }
                         break;
                 }
-
                 tortugas[i] = tortugas[i] + num;
                 if (tortugas[i]>=10){
                     System.out.println("\n\nGANA LA TROTUGA "+(i+1)+" vanzando "+num+" posiciones, llegando a "+tortugas[i]);
@@ -55,7 +53,6 @@ public class UF1A20 {
                 else{
                     System.out.println("Tortuga "+(i+1)+" avanza "+num+" posiciones");
                 }
-
             }
         }
     }
