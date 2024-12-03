219078f4fee0b97266c04c8648ffe8af407051ae
Hacha63 <capitan.hacha@gmail.com>
2024-12-03 12:53:45 +0100

    a

---
+++ src/main/java/com/mycompany/java1/InteliJ.java
@@ -0,0 +1,22 @@
+package com.mycompany.java1;
+import  java.util.Scanner;
+
+
+public class InteliJ {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introdueix la teva nota: ");
+        int nota = scanner.nextInt();
+
+        if (nota < 5){
+            System.out.println("Suspes");
+        } else if (nota == 5 || nota == 6){
+            System.out.println("Aprovat");
+        } else if (nota == 7 || nota == 8){
+            System.out.println("Notable");
+        } else if (nota == 9 || nota == 10){
+            System.out.println("Excelent");
+        }
+    }
+}
